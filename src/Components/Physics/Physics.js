5a12d6ba417a52991101f27e9ebb48d89419e19a
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-14 12:42:08 +0600

    Breadcrumb done

--- src/Components/Physics/Physics.js
+++ src/Components/Physics/Physics.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CourseImage from '../../assets/Ellipse-18.svg'
 import InsImage from '../../assets/Ellipse-19.svg'
 import CourseSummery from './CourseSummery';
-import { notification, noticeBoardData, counterCardDara, classSchedule, chapterData } from './Constant';
+import { noticeBoardData, counterCardDara, classSchedule, chapterData } from './Constant';
 import Clock from '../Clock/Clock';
 import { useNavigate } from 'react-router-dom';
 import CourseNotification from './CourseNotification';
@@ -13,10 +13,35 @@ const Physics = () => {
 
     return (
         <div className='container mx-auto'>
-            <h1 className='text-4xl font-bold text-[#042954] py-8'>Course <span className='text-4xl'>&#8688;</span> Physics</h1>
+
+            <nav className="flex text-[#042954] pt-8 pb-4" aria-label="Breadcrumb">
+                <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                    <li className="inline-flex items-center">
+                        <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                            <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                            Home
+                        </a>
+                    </li>
+
+                    <li aria-current="page">
+                        <div className="flex items-center">
+                            <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                            <span className="ml-1 cursor-pointer text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Course</span>
+                        </div>
+                    </li>
+                    <li aria-current="page">
+                        <div className="flex items-center">
+                            <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                            <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400 cursor-pointer">Physics</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+
+
             <div className="course_banner grid grid-cols-1 gap-10 md:grid-cols-2">
                 <div className="subject bg-white shadow-lg px-4 md:px-5 py-8 pb-28">
-                    <h1 className='text-6xl text-[#042954] font-semibold mb-6'>Physics</h1>
+                    <h1 className='text-5xl text-[#042954] font-semibold mb-6'>Physics</h1>
                     <div className="flex">
                         <div className="content">
                             <h1 className='title_text'>Class 9-10</h1>
