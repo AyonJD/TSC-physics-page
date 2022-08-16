fb74ad5ee9cf8e7bbecdec9df4fdffda85e52477
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-16 19:27:23 +0600

    CourseNotification CSS convert done

--- src/Components/Physics/CourseNotification.js
+++ src/Components/Physics/CourseNotification.js
@@ -4,15 +4,15 @@ import { notification } from './Constant';
 const CourseNotification = () => {
     return (
         <>
-            <div className="notification max-h-[448px] overflow-y-auto bg-white shadow-lg px-4 md:px-5 pb-8">
-                <h1 className='title_text sticky top-0 md:py-8 py-4 bg-white'>Course Notification</h1>
+            <div className="notification overflow-y-auto shadow-lg px-4 pb-8">
+                <h1 className='title_text course_notification sticky top-0 py-4'>Course Notification</h1>
                 {
                     notification.map((item, index) => {
                         return (
                             <div key={index}>
-                                <h1 className={`inline-bloxk mt-4 mb-2 text-white bg-accent w-fit px-4 py-1 rounded-full`}>16 June, 2022</h1>
-                                <h1 className='text-secondary w-fit font-medium hover:underline cursor-pointer'>{item.title}</h1>
-                                <small className='text-[#646464]'>Student Name / 5 minute ago</small>
+                                <h1 className={`inline-block mt-4 mb-2 course_notification_date w-fit px-4 py-1 rounded-full`}>16 June, 2022</h1>
+                                <h1 className='course_notification_title w-fit font-medium cursor-pointer'>{item.title}</h1>
+                                <small className=''>Student Name / 5 minute ago</small>
                                 <hr />
                             </div>
                         )
